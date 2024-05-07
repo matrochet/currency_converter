4d199c8e4ba7640007a5d029612f29e8b9abee2a
marochet <matthieu-rochet@outlook.com>
2024-05-07 19:33:43 +0200

    update

--- cc.js
+++ cc.js
@@ -35,49 +35,45 @@ const container = document.getElementById("container");
 // Add the fragment to the container
 container.appendChild(fragment);
 
-// Récupérer les deux champs
-// const eurgbpInput = document.getElementById("input-eurgbp");
-// const gbpeurInput = document.getElementById("input-gbpeur");
-//
-// Initialize the rate show at screen
-// gbpeurInput.value = CURRENCIES.gbp.toFixed(5);
-//
-// Associer un évènement qui correspond au changement de la valeur du champ
-// eurgbpInput.addEventListener("input", () => {
-// Récupérer la valeur saisir by user
-// const eur = eurgbpInput.value;
-// console.log(eur)
-//  Conertir la valeur en nombre
-// const eurNomber = parseFloat(eur);
-//  console.log(eur, typeof eur)
-// console.log(eurNomber, typeof eurNomber)
-// Calcul new conversion
-// const newConversion = eurNomber * CURRENCIES.gbp;
-// console.log(newConversion)
-// Converting new number in text with 5 digits
-// const newConversionTexte = newConversion.toFixed(5);
+const onChange = () => {
+ 
+}
 
-// console.log(newConversionTexte)
-// Update input
-// gbpeurInput.value = newConversionTexte;
-// });
-//
-// gbpeurInput.addEventListener("input", () => {
+// Collect all text input
+const inputs = document.querySelectorAll('input')
+
+console.log(inputs)
+
+// Associate a function when changing a input
+
+const eurgbpInput = document.getElementById("input-eurgbp");
+const gbpeurInput = document.getElementById("input-gbpeur");
+
+// Initialize the rate show at screen
+gbpeurInput.value = CURRENCIES.gbp.toFixed(5);
+
+// Associer un évènement qui correspond au changement de la valeur du champ
+eurgbpInput.addEventListener("input", () => {
+
 // Récupérer la valeur saisir by user
-// const gbp = gbpeurInput.value;
-// console.log(eur)
-// Conertir la valeur en nombre
-// const gbpNomber = parseFloat(gbp);
-// console.log(eur, typeof eur)
-// console.log(eurNomber, typeof eurNomber)
+const eur = eurgbpInput.value;
+console.log(eur)
+
+//  Convertir la valeur en nombre
+const eurNomber = parseFloat(eur);
+console.log(eur, typeof eur)
+console.log(eurNomber, typeof eurNomber)
+
 // Calcul new conversion
-// const newConversion = gbpNomber / CURRENCIES.gbp;
-//
-// console.log(newConversion);
+const newConversion = eurNomber * CURRENCIES.gbp;
+console.log(newConversion)
+
 // Converting new number in text with 5 digits
-// const newConversionTexte = newConversion.toFixed(5);
-//
-// console.log(newConversionTexte);
+const newConversionTexte = newConversion.toFixed(5);
+
+console.log(newConversionTexte)
+
 // Update input
-// eurgbpInput.value = newConversionTexte;
-//
+gbpeurInput.value = newConversionTexte;
+});
+
