ed136e6412d1ac988d74cfe2f7d65ea506bc7a66
pirocheto <pirocheto@gmail.com>
2024-05-20 01:00:33 +0200

    parse json data and fix data iteration

--- loading.js
+++ loading.js
@@ -1,111 +1,25 @@
 // Adress of rates from BCE in XML format
 // const adress = 'https://open.er-api.com/v6/latest/USD'
-const adress = 'currencies.json'
+const adress = "currencies.json";
 
 export const loadData = async () => {
-  const data = await fetch(adress)
-    .then(response => {
-      if (response.ok === false) {
-        return Promise.reject('Error in collect data')
-      }
+  const data = await fetch(adress).then((response) => {
+    if (response.ok === false) {
+      return Promise.reject("Error in collect data");
+    }
 
-      return response.text()
-    })
-    
-  console.log(data)
-  // console.log(data['rates']['AFN'])
+    return response.json();
+  });
 
   const CURRENCIES = {};
-
-  // Extraire data contenues dans les balises
-  // <Cube></Cube>
-
-  const BALISE_CUBE = "<Cube>";
-  const positionCubeEntree = data.indexOf(BALISE_CUBE);
-
-  // console.log(positionCubeEntree)
-
-  const BALISE_CUBE_FIN = "</Cube>";
-  const positionCubeSortie = data.lastIndexOf(BALISE_CUBE_FIN);
-
-  // console.log(positionCubeSortie)
-
-  const Taille_Balise_Fin = BALISE_CUBE_FIN.length;
-  const dataXML = data.substring(
-    positionCubeEntree,
-    positionCubeSortie + Taille_Balise_Fin
-  );
-
-  // console.log(dataXML)
-
-  // Manipuler XML data
-
-  // Créer un DomParser
-
-  const parser = new DOMParser();
-  const mineTypeXML = "text/xml";
-
-  // Converting text XML on DocumentXML
-
-  const documentXML = parser.parseFromString(dataXML, mineTypeXML);
-
-  console.log(documentXML);
-
-  // Browse our XML document
-
-  // Collect the first Cube tag
-
-  const cube1 = documentXML.firstElementChild;
-
-  console.log(cube1);
-
-  // Collect the second Cube tag embedded in the first
-
-  const cube2 = cube1.firstElementChild;
-
-  console.log(cube2);
-  console.log(cube2.attributes);
-
-  // Collect the attributes
-
-  const cubeAttributes = cube2.attributes;
-
-  // Collect the time attribute
-
-  const attrTime = cubeAttributes.getNamedItem("time");
-
-  console.log(attrTime);
-
-  const time = attrTime.value;
-
-  console.log(time);
-
-  // Shows date on screen
+  const time = data["time_last_update_utc"];
 
   const timeElement = document.getElementById("time");
   timeElement.textContent = `Dates of rates: ${time}`;
 
-  // Browse cube2 and collect all elements (cube)
-
-  const elements = cube2.children;
-
-  console.log(elements);
-
-  // To each element
-
-  for (const element of elements) {
-    // Collect currency and rate attribute
-
-    const attributs = element.attributes;
-    const currencyText = attributs.getNamedItem("currency").value;
-    const rateText = attributs.getNamedItem("rate").value;
-
-    const monnaie = currencyText.toLowerCase();
-    const taux = parseFloat(rateText);
-
-    // Add money at gloabl object
-
-    CURRENCIES[monnaie] = taux;
+  for (const [currency, rate] of Object.entries(data.rates)) {
+    const monnaie = currency.toLowerCase();
+    CURRENCIES[monnaie] = rate;
   }
   // console.log(CURRENCIES);
 
