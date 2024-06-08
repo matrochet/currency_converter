bd6f988e0be67fbfe696c8bf17d60fe083eba203
pirocheto <pirocheto@gmail.com>
2024-06-08 21:44:26 +0200

    add event on input value component

--- convert.js
+++ convert.js
@@ -1,11 +1,25 @@
 import { loadData } from "./loading.js";
 
+// Get input components
+const inputCurrency = document.getElementById("input-currency");
+const outputCurrency = document.getElementById("output-currency");
+
+const inputValue = document.getElementById("input-value");
+const outputValue = document.getElementById("output-value");
+
+inputValue.addEventListener("input", (event) => {
+  const value = event.data;
+  const currency = inputCurrency.value;
+
+  // Apply real function to convert currency here
+  const valueConvert = value * 1.5;
+  // --------------------------------------------
+
+  outputValue.value = valueConvert;
+});
+
 const init = async () => {
   const data = await loadData();
-
-  // Get input components
-  const inputCurrency = document.getElementById("input-currency");
-  const outputCurrency = document.getElementById("output-currency");
 
   // Iter on data
   for (const [currency, value] of Object.entries(data)) {
